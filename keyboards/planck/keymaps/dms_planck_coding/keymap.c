ca0cc6b6437d12e7841588cdb0e9cfc260fe5c6e
Daniel Stack <daniel@stackfamily.org>
2022-03-03 21:10:32 -0500

    added preonic build

--- keyboards/planck/keymaps/dms_planck_coding/keymap.c
+++ keyboards/planck/keymaps/dms_planck_coding/keymap.c
@@ -12,6 +12,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * qmk compile -kb planck/rev6 -km dms_planck_coding
  */
 
 #include QMK_KEYBOARD_H
